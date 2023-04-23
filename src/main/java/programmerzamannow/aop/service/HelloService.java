c73638a083650cf922ff5e126a0d9ebdb3d2f3a3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 21:52:36 +0700

    Tanpa AOP

---
+++ src/main/java/programmerzamannow/aop/service/HelloService.java
@@ -0,0 +1,20 @@
+package programmerzamannow.aop.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class HelloService {
+
+    public String hello(String name) {
+        log.info("Call HelloService.hello()");
+        return "Hello " + name;
+    }
+
+    public String bye(String name) {
+        log.info("Call HelloService.bye()");
+        return "Bye " + name;
+    }
+
+}
---
+++ src/test/java/programmerzamannow/aop/service/HelloServiceTest.java
@@ -0,0 +1,19 @@
+package programmerzamannow.aop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloServiceTest {
+
+    @Autowired
+    private HelloService helloService;
+
+    @Test
+    void helloService() {
+        Assertions.assertEquals("Hello Eko", helloService.hello("Eko"));
+        Assertions.assertEquals("Bye Eko", helloService.bye("Eko"));
+    }
+}
