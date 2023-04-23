b588157d67d47575eda20d813733347c582528fb
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 21:40:56 +0700

    belajar spring aop

---
+++ src/main/java/programmerzamannow/aop/BelajarSpringAopApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
