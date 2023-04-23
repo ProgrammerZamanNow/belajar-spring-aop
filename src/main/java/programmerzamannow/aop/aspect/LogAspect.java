bca5d2befc805857e4c0330e793799ceae3d9c61
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 22:02:37 +0700

    Pointcut

---
+++ src/main/java/programmerzamannow/aop/aspect/LogAspect.java
@@ -0,0 +1,15 @@
+package programmerzamannow.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    @Pointcut("")
+    public void helloServiceMethod(){
+
+    }
+}
