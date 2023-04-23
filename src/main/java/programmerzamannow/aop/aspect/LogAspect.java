c8a3c144d4b7e9c67a24afdc18fba7d721677be1
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 22:08:13 +0700

    Pointcut Expression

--- src/main/java/programmerzamannow/aop/aspect/LogAspect.java
+++ src/main/java/programmerzamannow/aop/aspect/LogAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Pointcut("")
+    @Pointcut("target(programmerzamannow.aop.service.HelloService)")
     public void helloServiceMethod(){
 
     }
