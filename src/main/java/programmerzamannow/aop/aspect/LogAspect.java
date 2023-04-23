9e50daa782eb0b194b9ce77fed668f5ac915f683
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 22:15:59 +0700

    Advice

--- src/main/java/programmerzamannow/aop/aspect/LogAspect.java
+++ src/main/java/programmerzamannow/aop/aspect/LogAspect.java
@@ -1,15 +1,28 @@
 package programmerzamannow.aop.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
 
     @Pointcut("target(programmerzamannow.aop.service.HelloService)")
     public void helloServiceMethod(){
 
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod(){
+        log.info("Before HelloService Method");
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod2(){
+        log.info("Before HelloService Method Again");
     }
 }
