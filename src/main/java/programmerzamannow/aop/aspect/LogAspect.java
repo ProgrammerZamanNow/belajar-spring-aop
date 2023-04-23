3c2d8cb0cc470ddc2c71bfbecacb7ba8d9ac0dcd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-04-23 22:27:00 +0700

    Proceeding Join Point

--- src/main/java/programmerzamannow/aop/aspect/LogAspect.java
+++ src/main/java/programmerzamannow/aop/aspect/LogAspect.java
@@ -2,6 +2,8 @@ package programmerzamannow.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,19 +15,29 @@ import org.springframework.stereotype.Component;
 public class LogAspect {
 
     @Pointcut("target(programmerzamannow.aop.service.HelloService)")
-    public void helloServiceMethod(){
+    public void helloServiceMethod() {
 
     }
 
     @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethod(JoinPoint joinPoint){
+    public void beforeHelloServiceMethod(JoinPoint joinPoint) {
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
         log.info("Before " + className + "." + methodName + "()");
     }
 
-    @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethod2(){
-        log.info("Before HelloService Method Again");
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        try {
+            log.info("Around Before " + className + "." + methodName + "()");
+            return joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            log.info("Around Error " + className + "." + methodName + "()");
+            throw throwable;
+        } finally {
+            log.info("Around Finally " + className + "." + methodName + "()");
+        }
     }
 }
